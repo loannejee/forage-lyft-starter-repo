97c32bf0b26c42d96423aaa9f2f520bd22c5c152
Joanne <jl7105@nyu.edu>
2022-07-20 23:39:52 -0400

    not sure if i need abstract

--- engine/engine.py
+++ engine/engine.py
@@ -1,10 +1,8 @@
 from car import Car
-from abc import ABC, abstractmethod
 
-class Engine(Car, ABC):
+class Engine(Car):
     def __init__(self):
         pass
 
-    @abstractmethod
     def needs_service(self):
         pass
