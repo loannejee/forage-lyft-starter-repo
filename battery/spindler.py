662813c292fb82f62ec8f39f58fc913b7695907b
Joanne <jl7105@nyu.edu>
2022-07-22 13:59:33 -0400

    updated service year requirement for battery

--- battery/spindler.py
+++ battery/spindler.py
@@ -7,7 +7,11 @@ class Spindler(Battery):
         self.last_service_date = last_service_date
 
     def needs_service(self):
-        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 2)
+        # date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 2)
+
+        # For task 3: modify the Spindler battery so it requires service after three years instead of two
+        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 3)
+        
         if date_which_battery_should_be_serviced_by < self.current_date:
             return True
         else:
